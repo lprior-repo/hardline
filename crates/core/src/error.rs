3e9f14ea98a0d28ea01375588aa51a6c1cee6304
Lewis <priorlewis43@gmail.com>
2026-04-28 07:46:57 -0500

    fix: replace io::Error::new with io::Error::other (clippy)

--- crates/core/src/error.rs
+++ crates/core/src/error.rs
@@ -126,8 +126,7 @@ impl From<std::io::Error> for Error {
 impl From<serde_json::Error> for Error {
     fn from(e: serde_json::Error) -> Self {
         use super::error_io::IoErrorKind;
-        IoErrorKind::Io(std::io::Error::new(
-            std::io::ErrorKind::Other,
+        IoErrorKind::Io(std::io::Error::other(
             format!("JSON error: {e}"),
         ))
         .into()
